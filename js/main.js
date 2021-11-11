bfb9fabe893f9ee4507f15f8f44a15056d7a2669
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-11 14:45:23 +0100

    insert title and img

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-
+/**
+ * Stampare a schermo un messaggio all’interno di un h1, utilizzando i data di Vue.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
 
 
 // creazione instanza Vue
 
 const app = new Vue({
+    el: '#app',
      // struttura dati
-
+   
      data: {
-         
+         title: 'Benvenuto Vue',
+         image: 'https://picsum.photos/200/300'
      }
 })
